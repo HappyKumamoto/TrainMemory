67bd2fbfbe3174c6039600cd71cd6481eb4ee0fe
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2024-04-15 00:34:46 +0900

    Update memory.js

--- memory.js
+++ memory.js
@@ -40,7 +40,7 @@ let img_arr = [
     "img/suigou","img/suisei","img/tanigawaL","img/tanigawa",
     "img/tohkai","img/toki","img/tsubame","img/tsubasa",
     "img/uzushio","img/yakumoL","img/yamabiko","img/hakutsuruR",
-    "img/hatsukariR1","img/hatsukariR2","img/kokutetsudoori1"
+    "img/hatsukariR1","img/hatsukariR2","img/kokutetsudoori1","img/kamomeR"
     /*,"img/","img/"*/
     
 ];
