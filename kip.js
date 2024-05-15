ed0bfaaaf6cd153a6152a7a437c29e88ab49ab2f
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2024-05-15 14:12:46 +0900

    Update kip.js

--- kip.js
+++ kip.js
@@ -6,7 +6,7 @@ let kip = document.getElementsByClassName('kip');
 //let oncli = document.createElement("script");   //onclick.jsを持ってくる
 //oncli.src = "onclick.js";
 
-//絵文字を作る関数。n は個数
+//画像を作る関数。n は個数
 function kipMaker(n) {
     let kip = document.createElement("div");
     kip.className = "kip";
@@ -17,18 +17,18 @@ function kipMaker(n) {
     }
 }
 
-//絵文字のセッティングをする関数。
+//画像のセッティングをする関数。
 function kipSet(clone) {
     let kipClone = clone.cloneNode(true);
     let kipStyle = kipClone.style;
 
-    //絵文字の位置（left）、時間をずらす（animation-delay）、サイズ（font-size）をランダムで指定
+    //画像の位置（left）、時間をずらす（animation-delay）、サイズ（font-size）をランダムで指定
     kipStyle.left = 100 * Math.random() + "%";
     kipStyle.animationDelay = 12 * Math.random() + "s";
     kipStyle.fontSize = Math.floor(50 * Math.random() + 20) + "px";
     document.body.appendChild(kipClone);
 
-    //一つのアニメーションが終わったら新しい絵文字を生成
+    //一つのアニメーションが終わったら新しい画像を生成
     kipClone.addEventListener("animationend", function() {
         this.parentNode.removeChild(this);
         let kip = document.createElement("div");
@@ -39,5 +39,5 @@ function kipSet(clone) {
     }, false)
 }
 
-//絵文字を50個降らせる
-kipMaker(50)
+//画像を50個降らせる
+kipMaker(5)
